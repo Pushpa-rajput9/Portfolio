1fdc56d4e35c23caabeb3f68b0a4552c15085c00
pushpa rajput <pr4541285@gmail.com>
2024-06-23 21:57:30 +0530

    Add projecs

--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -6,6 +6,241 @@ function Projects() {
           My Projects
         </button>
       </div>
+      <div className="main pl-24 pr-24">
+        <div className="flex flex-wrap gap-20">
+          <div className="max-w-xl bg-white rounded-lg border border-gray-200 shadow-lg dark:bg-gray-800 dark:border-gray-700 my-8">
+            <a
+              target="_blank"
+              href=" https://pushpa-rajput9.github.io/AmazonClone/"
+            >
+              <img
+                className="rounded-t-lg w-full"
+                src="https://images.wallpaperscraft.com/image/single/system_scheme_blue_blueprint_10428_300x168.jpg"
+                alt=""
+              />
+            </a>
+            <div className="p-5">
+              <a
+                target="_blank"
+                href=" https://pushpa-rajput9.github.io/AmazonClone/"
+              >
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Amazon Clone
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                I built an Amazon clone which have basic functionalities like
+                Add to Cart , you can check your order by visit order page .
+                This project is built through React js, React router, Tailwind
+                Css.
+              </p>
+              <a
+                target="_blank"
+                href=" https://pushpa-rajput9.github.io/AmazonClone/"
+                className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-blue-300"
+              >
+                Read more
+                <svg
+                  className="ml-2 -mr-1 w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div className="max-w-xl bg-white rounded-lg border border-gray-200 shadow-lg dark:bg-gray-800 dark:border-gray-700 my-8">
+            <a
+              target="_blank"
+              href="https://pushpa-rajput9.github.io/Calculator/"
+            >
+              <img
+                className="rounded-t-lg w-full"
+                src="https://images.wallpaperscraft.com/image/single/system_scheme_blue_blueprint_10428_300x168.jpg"
+                alt=""
+              />
+            </a>
+            <div className="p-5">
+              <a
+                target="_blank"
+                href="https://pushpa-rajput9.github.io/Calculator/"
+              >
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Calculator
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                🚀 Just built a calculator using React! 🧮🔥 🔹 Tech Stack:
+                JavaScript HTML Tailwind CSS <br />
+                🔹 Features: <br /> 1. Arithmetic operations (addition,
+                subtraction, multiplication, division) <br /> 2. Scientific
+                operations (sin, log, cos, tan, root, Exp) <br /> 3. Clear
+                button to reset <br /> 4. Responsive design
+              </p>
+              <a
+                target="_blank"
+                href="https://pushpa-rajput9.github.io/Calculator/"
+                className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-blue-300"
+              >
+                Read more
+                <svg
+                  className="ml-2 -mr-1 w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div className="max-w-xl bg-white rounded-lg border border-gray-200 shadow-lg dark:bg-gray-800 dark:border-gray-700 my-8">
+            <a target="_blank" href="https://pushpa-rajput9.github.io/TODO/">
+              <img
+                className="rounded-t-lg w-full"
+                src="https://images.wallpaperscraft.com/image/single/system_scheme_blue_blueprint_10428_300x168.jpg"
+                alt=""
+              />
+            </a>
+            <div className="p-5">
+              <a target="_blank" href="https://pushpa-rajput9.github.io/TODO/">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  TO-DO List
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                I built an TO-DO list project with Local Storage. This project
+                is based on context API. It is built using JavaScript, React JS,
+                React router, Tailwind CSS.
+              </p>
+              <a
+                target="_blank"
+                href="https://pushpa-rajput9.github.io/TODO/"
+                className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-blue-300"
+              >
+                Read more
+                <svg
+                  className="ml-2 -mr-1 w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div className="max-w-xl bg-white rounded-lg border border-gray-200 shadow-lg dark:bg-gray-800 dark:border-gray-700 my-8">
+            <a
+              target="_blank"
+              href="https://pushpa-rajput9.github.io/Currency-Convertor-/"
+            >
+              <img
+                className="rounded-t-lg w-full"
+                src="https://images.wallpaperscraft.com/image/single/system_scheme_blue_blueprint_10428_300x168.jpg"
+                alt=""
+              />
+            </a>
+            <div className="p-5">
+              <a
+                target="_blank"
+                href="https://pushpa-rajput9.github.io/Currency-Convertor-/"
+              >
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Currency Converter
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                I built an Currency Converter through which you can check money
+                value of any country by comparing the Currency. This project is
+                based on context API. It is built using JavaScript, React JS,
+                React router, Tailwind CSS.
+              </p>
+              <a
+                target="_blank"
+                href="https://pushpa-rajput9.github.io/Currency-Convertor-/"
+                className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-blue-300"
+              >
+                Read more
+                <svg
+                  className="ml-2 -mr-1 w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div className="max-w-xl bg-white rounded-lg border border-gray-200 shadow-lg dark:bg-gray-800 dark:border-gray-700 my-8">
+            <a
+              target="_blank"
+              href=" https://incandescent-pudding-e15f8a.netlify.app/"
+            >
+              <img
+                className="rounded-t-lg w-full"
+                src="https://images.wallpaperscraft.com/image/single/system_scheme_blue_blueprint_10428_300x168.jpg"
+                alt=""
+              />
+            </a>
+            <div className="p-5">
+              <a
+                target="_blank"
+                href=" https://incandescent-pudding-e15f8a.netlify.app/"
+              >
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Beats Project
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                The Multi-Page Beats Project with Headphone, Earbuds, and
+                Accessories is a web-based project that is built using HTML and
+                CSS. This project consists of several web pages that are
+                designed to showcase a wide variety of audio products and
+                accessories available for purchase online by customers.
+              </p>
+              <a
+                target="_blank"
+                href=" https://incandescent-pudding-e15f8a.netlify.app/"
+                className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-blue-300"
+              >
+                Read more
+                <svg
+                  className="ml-2 -mr-1 w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
